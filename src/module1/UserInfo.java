43bd5312b7136797eed0119d204d9f9391756e31
Berat <berat.avci@csuglobal.edu>
2026-09-20 17:53:18 -0400

    test3

--- src/module1/UserInfo.java
+++ src/module1/UserInfo.java
@@ -3,10 +3,21 @@ package module1;
 public class UserInfo {
 	public static void main (String [] args) {
 		String firstName;
+		String lastName;
+		String streetAddress;
+		String cityName;
+		int zipcodeNumber;
 		
-		firstName = "Tyler";
+		firstName = "Billy";
+		lastName = "Ayers";
+		streetAddress = "336 Bates Brothers Road";
+		cityName = "Columbus";
+		zipcodeNumber = 43209;
 		
 		System.out.println("First Name: " + firstName);
-		
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Street Address: " + streetAddress);
+		System.out.println("City: " + cityName);
+		System.out.println("Zipcode: " + zipcodeNumber);
 	}
 }
