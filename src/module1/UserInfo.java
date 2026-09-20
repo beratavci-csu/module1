ac4e99a0e14d3afc15ae1d33d2293fe85eb4a838
Berat <berat.avci@csuglobal.edu>
2026-09-20 17:43:15 -0400

    test2

---
+++ src/module1/UserInfo.java
@@ -0,0 +1,12 @@
+package module1;
+
+public class UserInfo {
+	public static void main (String [] args) {
+		String firstName;
+		
+		firstName = "Tyler";
+		
+		System.out.println("First Name: " + firstName);
+		
+	}
+}
